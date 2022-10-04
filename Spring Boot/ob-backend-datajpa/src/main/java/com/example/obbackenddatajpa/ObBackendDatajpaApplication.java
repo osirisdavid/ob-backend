d6a85634f483e6facfb2999a1518166e04800989
osirisdavid <davidosirisayala@gmail.com>
2022-10-03 23:42:27 -0300

    Spring java 17

---
+++ Spring Boot/ob-backend-datajpa/src/main/java/com/example/obbackenddatajpa/ObBackendDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.obbackenddatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObBackendDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObBackendDatajpaApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/ob-backend-datajpa/src/test/java/com/example/obbackenddatajpa/ObBackendDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obbackenddatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObBackendDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
