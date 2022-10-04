23c45953d0d696e3ab93d22408b558502495767f
osirisdavid <davidosirisayala@gmail.com>
2022-10-03 23:58:51 -0300

    CRUD

--- Spring Boot/ob-backend-datajpa/src/main/java/com/example/obbackenddatajpa/ObBackendDatajpaApplication.java
+++ Spring Boot/ob-backend-datajpa/src/main/java/com/example/obbackenddatajpa/ObBackendDatajpaApplication.java
@@ -1,13 +1,35 @@
 package com.example.obbackenddatajpa;
 
+import com.example.obbackenddatajpa.entities.Book;
+import com.example.obbackenddatajpa.repository.BookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ObBackendDatajpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObBackendDatajpaApplication.class, args);
+		//SpringApplication.run(ObBackendDatajpaApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ObBackendDatajpaApplication.class, args); //Nos da el contenedor de Beans
+		BookRepository repository = context.getBean(BookRepository.class);
+
+		//CRUD
+		//crear un libro
+		Book book1 = new Book(null, "Homo Deus", "Yuval Noah", 450, 29.99, LocalDate.of(2022,9,9), true);
+		Book book2 = new Book(null, "Homo Sapiens", "Yuval Noah", 450, 29.99, LocalDate.of(2013,9,9), true);
+
+		//almacenar un libro
+		repository.save(book1);
+		repository.save(book2);
+
+		//recuperar un libre
+		System.out.println(repository.findAll());
+
+		//borrar un libro
+		//repository.deleteById(1L);
 	}
 
 }
---
+++ Spring Boot/ob-backend-datajpa/src/main/java/com/example/obbackenddatajpa/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.obbackenddatajpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * Configuracion Swagger para la generacion de documentacion de la API REST
+ *
+ * http://localhost:8080/swagger-ui/
+ */
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Spring Boot Book API REST",
+                "Library Api rest docs",
+                "1.0",
+                "http://www.google.com",
+                new Contact("David", "http://www.google.com", "davidosirisayala@gmail.com"),
+                "MIT",
+                "http://www.google.com",
+                Collections.emptyList());
+    }
+}
